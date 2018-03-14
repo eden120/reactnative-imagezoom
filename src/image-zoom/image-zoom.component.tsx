e3fe5bd6fe81214e27d826e0bb66edc280ca8ffd
jcb <jcb@mains-iMac.local>
2018-03-14 06:41:05 -0700

    bug fix

--- src/image-zoom/image-zoom.component.tsx
+++ src/image-zoom/image-zoom.component.tsx
@@ -26,72 +26,56 @@ export default class ImageViewer extends React.Component<Props, State> {
   public static defaultProps = new Props()
   public state = new State()
 
-  // 上次/当前/动画 x 位移
   private lastPositionX: number | null = null
   private positionX = 0
   private animatedPositionX = new Animated.Value(0)
 
-  // 上次/当前/动画 y 位移
   private lastPositionY: number | null = null
   private positionY = 0
   private animatedPositionY = new Animated.Value(0)
 
-  // 缩放大小
   private scale = 1
   private animatedScale = new Animated.Value(1)
   private zoomLastDistance: number | null = null
   private zoomCurrentDistance = 0
 
-  // 图片手势处理
   private imagePanResponder: PanResponderInstance | null = null
 
-  // 图片视图当前中心的位置
   // private centerX: number
   // private centerY: number
 
-  // 上次手按下去的时间
   private lastTouchStartTime: number = 0
 
-  // 滑动过程中，整体横向过界偏移量
   private horizontalWholeOuterCounter = 0
 
-  // 滑动过程中，x y的总位移
   private horizontalWholeCounter = 0
   private verticalWholeCounter = 0
 
-  // 两手距离中心点位置
   private centerDiffX = 0
   private centerDiffY = 0
 
-  // 触发单击的 timeout
   private singleClickTimeout: any
 
-  // 计算长按的 timeout
   private longPressTimeout: any
 
-  // 上一次点击的时间
   private lastClickTime = 0
 
-  // 双击时的位置
   private doubleClickX = 0
   private doubleClickY = 0
 
-  // 是否双击了
   private isDoubleClick = false
 
-  // 是否是长按
   private isLongPress = false
 
   public componentWillMount() {
     const setResponder = isMobile()
 
     this.imagePanResponder = PanResponder.create({
-      // 要求成为响应者：
+
       onStartShouldSetPanResponder: (evt, gestureState) => setResponder,
       onPanResponderTerminationRequest: (evt, gestureState) => false,
 
       onPanResponderGrant: (evt, gestureState) => {
-        // 开始手势操作
         this.lastPositionX = null
         this.lastPositionY = null
         this.zoomLastDistance = null
@@ -101,7 +85,6 @@ export default class ImageViewer extends React.Component<Props, State> {
         this.isDoubleClick = false
         this.isLongPress = false
 
-        // 任何手势开始，都清空单击计时器
         if (this.singleClickTimeout) {
           clearTimeout(this.singleClickTimeout)
         }
@@ -120,7 +103,6 @@ export default class ImageViewer extends React.Component<Props, State> {
           this.centerDiffY = centerY - this.props.cropHeight / 2
         }
 
-        // 计算长按
         if (this.longPressTimeout) {
           clearTimeout(this.longPressTimeout)
         }
@@ -132,45 +114,34 @@ export default class ImageViewer extends React.Component<Props, State> {
         }, this.props.longPressTime)
 
         if (evt.nativeEvent.changedTouches.length <= 1) {
-          // 一个手指的情况
           if (
             new Date().getTime() - this.lastClickTime <
             (this.props.doubleClickInterval || 0)
           ) {
-            // 认为触发了双击
+
             this.lastClickTime = 0
             if (this.props.onDoubleClick) {
               this.props.onDoubleClick()
             }
 
-            // 取消长按
             clearTimeout(this.longPressTimeout)
 
-            // 因为可能触发放大，因此记录双击时的坐标位置
             this.doubleClickX = evt.nativeEvent.changedTouches[0].pageX
             this.doubleClickY = evt.nativeEvent.changedTouches[0].pageY
 
-            // 缩放
             this.isDoubleClick = true
             if (this.scale > 1 || this.scale < 1) {
-              // 回归原位
               this.scale = 1
 
               this.positionX = 0
               this.positionY = 0
             } else {
-              // 开始在位移地点缩放
-              // 记录之前缩放比例
-              // 此时 this.scale 一定为 1
               const beforeScale = this.scale
 
-              // 开始缩放
               this.scale = 2
 
-              // 缩放 diff
               const diffScale = this.scale - beforeScale
-              // 找到两手中心点距离页面中心的位移
-              // 移动位置
+
               this.positionX =
                 (this.props.cropWidth / 2 - this.doubleClickX) *
                 diffScale /
@@ -203,23 +174,19 @@ export default class ImageViewer extends React.Component<Props, State> {
       },
       onPanResponderMove: (evt, gestureState) => {
         if (this.isDoubleClick) {
-          // 有时双击会被当做位移，这里屏蔽掉
           return
         }
 
         if (evt.nativeEvent.changedTouches.length <= 1) {
-          // x 位移
           let diffX = gestureState.dx - (this.lastPositionX || 0)
           if (this.lastPositionX === null) {
             diffX = 0
           }
-          // y 位移
           let diffY = gestureState.dy - (this.lastPositionY || 0)
           if (this.lastPositionY === null) {
             diffY = 0
           }
 
-          // 保留这一次位移作为下次的上一次位移
           this.lastPositionX = gestureState.dx
           this.lastPositionY = gestureState.dy
 
@@ -230,26 +197,18 @@ export default class ImageViewer extends React.Component<Props, State> {
             Math.abs(this.horizontalWholeCounter) > 5 ||
             Math.abs(this.verticalWholeCounter) > 5
           ) {
-            // 如果位移超出手指范围，取消长按监听
+
             clearTimeout(this.longPressTimeout)
           }
 
           if (this.props.panToMove) {
-            // diffX > 0 表示手往右滑，图往左移动，反之同理
-            // horizontalWholeOuterCounter > 0 表示溢出在左侧，反之在右侧，绝对值越大溢出越多
             if (this.props.imageWidth * this.scale > this.props.cropWidth) {
-              // 如果图片宽度大图盒子宽度， 可以横向拖拽
-              // 没有溢出偏移量或者这次位移完全收回了偏移量才能拖拽
               if (this.horizontalWholeOuterCounter > 0) {
-                // 溢出在右侧
                 if (diffX < 0) {
-                  // 从右侧收紧
                   if (this.horizontalWholeOuterCounter > Math.abs(diffX)) {
-                    // 偏移量还没有用完
                     this.horizontalWholeOuterCounter += diffX
                     diffX = 0
                   } else {
-                    // 溢出量置为0，偏移量减去剩余溢出量，并且可以被拖动
                     diffX += this.horizontalWholeOuterCounter
                     this.horizontalWholeOuterCounter = 0
                     if (this.props.horizontalOuterRangeOffset) {
@@ -257,19 +216,14 @@ export default class ImageViewer extends React.Component<Props, State> {
                     }
                   }
                 } else {
-                  // 向右侧扩增
                   this.horizontalWholeOuterCounter += diffX
                 }
               } else if (this.horizontalWholeOuterCounter < 0) {
-                // 溢出在左侧
                 if (diffX > 0) {
-                  // 从左侧收紧
                   if (Math.abs(this.horizontalWholeOuterCounter) > diffX) {
-                    // 偏移量还没有用完
                     this.horizontalWholeOuterCounter += diffX
                     diffX = 0
                   } else {
-                    // 溢出量置为0，偏移量减去剩余溢出量，并且可以被拖动
                     diffX += this.horizontalWholeOuterCounter
                     this.horizontalWholeOuterCounter = 0
                     if (this.props.horizontalOuterRangeOffset) {
@@ -277,42 +231,31 @@ export default class ImageViewer extends React.Component<Props, State> {
                     }
                   }
                 } else {
-                  // 向左侧扩增
                   this.horizontalWholeOuterCounter += diffX
                 }
               } else {
-                // 溢出偏移量为0，正常移动
               }
 
-              // 产生位移
               this.positionX += diffX / this.scale
 
-              // 但是横向不能出现黑边
-              // 横向能容忍的绝对值
               const horizontalMax =
                 (this.props.imageWidth * this.scale - this.props.cropWidth) /
                 2 /
                 this.scale
               if (this.positionX < -horizontalMax) {
-                // 超越了左边临界点，还在继续向左移动
                 this.positionX = -horizontalMax
 
-                // 让其产生细微位移，偏离轨道
                 this.horizontalWholeOuterCounter += -1 / 1e10
               } else if (this.positionX > horizontalMax) {
-                // 超越了右侧临界点，还在继续向右移动
                 this.positionX = horizontalMax
 
-                // 让其产生细微位移，偏离轨道
                 this.horizontalWholeOuterCounter += 1 / 1e10
               }
               this.animatedPositionX.setValue(this.positionX)
             } else {
-              // 不能横向拖拽，全部算做溢出偏移量
               this.horizontalWholeOuterCounter += diffX
             }
 
-            // 溢出量不会超过设定界限
             if (
               this.horizontalWholeOuterCounter > (this.props.maxOverflow || 0)
             ) {
@@ -324,7 +267,6 @@ export default class ImageViewer extends React.Component<Props, State> {
             }
 
             if (this.horizontalWholeOuterCounter !== 0) {
-              // 如果溢出偏移量不是0，执行溢出回调
               if (this.props.horizontalOuterRangeOffset) {
                 this.props.horizontalOuterRangeOffset(
                   this.horizontalWholeOuterCounter
@@ -333,20 +275,16 @@ export default class ImageViewer extends React.Component<Props, State> {
             }
 
             if (this.props.imageHeight * this.scale > this.props.cropHeight) {
-              // 如果图片高度大图盒子高度， 可以纵向拖拽
               this.positionY += diffY / this.scale
               this.animatedPositionY.setValue(this.positionY)
             }
           }
         } else {
-          // 多个手指的情况
-          // 取消长按状态
           if (this.longPressTimeout) {
             clearTimeout(this.longPressTimeout)
           }
 
           if (this.props.pinchToZoom) {
-            // 找最小的 x 和最大的 x
             let minX: number
             let maxX: number
             if (
@@ -392,18 +330,13 @@ export default class ImageViewer extends React.Component<Props, State> {
                 zoom = 10
               }
 
-              // 记录之前缩放比例
               const beforeScale = this.scale
 
-              // 开始缩放
               this.scale = zoom
               this.animatedScale.setValue(this.scale)
 
-              // 图片要慢慢往两个手指的中心点移动
-              // 缩放 diff
               const diffScale = this.scale - beforeScale
-              // 找到两手中心点距离页面中心的位移
-              // 移动位置
+
               this.positionX -= this.centerDiffX * diffScale / this.scale
               this.positionY -= this.centerDiffY * diffScale / this.scale
               this.animatedPositionX.setValue(this.positionX)
@@ -416,22 +349,18 @@ export default class ImageViewer extends React.Component<Props, State> {
         this.imageDidMove("onPanResponderMove")
       },
       onPanResponderRelease: (evt, gestureState) => {
-        // 取消长按
         if (this.longPressTimeout) {
           clearTimeout(this.longPressTimeout)
         }
 
-        // 双击结束，结束尾判断
         if (this.isDoubleClick) {
           return
         }
 
-        // 长按结束，结束尾判断
         if (this.isLongPress) {
           return
         }
 
-        // 如果是单个手指、距离上次按住大于预设秒、滑动距离小于预设值, 则可能是单击（如果后续双击间隔内没有开始手势）
         // const stayTime = new Date().getTime() - this.lastTouchStartTime!
         const moveDistance = Math.sqrt(
           gestureState.dx * gestureState.dx + gestureState.dy * gestureState.dy
@@ -446,7 +375,6 @@ export default class ImageViewer extends React.Component<Props, State> {
             }
           }, this.props.doubleClickInterval)
         } else {
-          // 多手势结束，或者滑动结束
           if (this.props.responderRelease) {
             this.props.responderRelease(gestureState.vx, this.scale)
           }
@@ -462,7 +390,6 @@ export default class ImageViewer extends React.Component<Props, State> {
 
   public panResponderReleaseResolve = () => {
     if (this.scale < 1) {
-      // 如果缩放小于1，强制重置为 1
       this.scale = 1
       Animated.timing(this.animatedScale, {
         toValue: this.scale,
@@ -471,7 +398,6 @@ export default class ImageViewer extends React.Component<Props, State> {
     }
 
     if (this.props.imageWidth * this.scale <= this.props.cropWidth) {
-      // 如果图片宽度小于盒子宽度，横向位置重置
       this.positionX = 0
       Animated.timing(this.animatedPositionX, {
         toValue: this.positionX,
@@ -480,7 +406,6 @@ export default class ImageViewer extends React.Component<Props, State> {
     }
 
     if (this.props.imageHeight * this.scale <= this.props.cropHeight) {
-      // 如果图片高度小于盒子高度，纵向位置重置
       this.positionY = 0
       Animated.timing(this.animatedPositionY, {
         toValue: this.positionY,
@@ -488,10 +413,7 @@ export default class ImageViewer extends React.Component<Props, State> {
       }).start()
     }
 
-    // 横向肯定不会超出范围，由拖拽时控制
-    // 如果图片高度大于盒子高度，纵向不能出现黑边
     if (this.props.imageHeight * this.scale > this.props.cropHeight) {
-      // 纵向能容忍的绝对值
       const verticalMax =
         (this.props.imageHeight * this.scale - this.props.cropHeight) /
         2 /
@@ -507,7 +429,6 @@ export default class ImageViewer extends React.Component<Props, State> {
       }).start()
     }
 
-    // 拖拽正常结束后,如果没有缩放,直接回到0,0点
     if (this.scale === 1) {
       this.positionX = 0
       this.positionY = 0
@@ -521,7 +442,6 @@ export default class ImageViewer extends React.Component<Props, State> {
       }).start()
     }
 
-    // 水平溢出量置空
     this.horizontalWholeOuterCounter = 0
 
     this.imageDidMove("onPanResponderRelease")
@@ -591,9 +511,6 @@ export default class ImageViewer extends React.Component<Props, State> {
     })
   }
 
-  /**
-   * 图片区域视图渲染完毕
-   */
   public handleLayout(event: LayoutChangeEvent) {
     // this.centerX = event.nativeEvent.layout.x + event.nativeEvent.layout.width / 2
     // this.centerY = event.nativeEvent.layout.y + event.nativeEvent.layout.height / 2
@@ -602,9 +519,6 @@ export default class ImageViewer extends React.Component<Props, State> {
     }
   }
 
-  /**
-   * 重置大小和位置
-   */
   public reset() {
     this.scale = 1
     this.animatedScale.setValue(this.scale)
